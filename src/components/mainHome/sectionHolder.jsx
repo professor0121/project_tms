439460961a6938471255aaf008bd0452fda6edff
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-03-31 21:18:50 +0530

    section2 added

--- src/components/mainHome/sectionHolder.jsx
+++ src/components/mainHome/sectionHolder.jsx
@@ -2,17 +2,27 @@ import React from 'react'
 
 const SectionHolder = () => {
     return (
-        <div className='bg-[#ffffff]'>
-            <div className="bg-[url('/promo0.png')] bg-no-repeat  bg-center h-96 w-full">
+        <div className='bg-[#ffffff] py-8'>
+            <div className="bg-[url('/promo0.png')] bg-no-repeat  bg-start- h-96 w-full" style={{backgroundPosition:'left center'}}>
                 <div className=' md:w-[1200px] w-full mx-auto pt-[15px] items-around'>
                     <div>
                         <h3 className='text-[30px] uppercase font-[700] text-[#191919]'>Why Travel with travol</h3>
                     </div>
-                    <div className='flex gap-8'>
-                        <div className='flex flex-col w-[400px] h-[220px] items-center justify-around'>
-                            <img className='h-[80px] w-[80px] '  src="https://placehold.co/80x80" alt="img" />
+                    <div className='flex md:flex-row flex-col gap-16 pt-[80px]'>
+                        <div className='flex flex-col w-[400px] h-[220px] items-center justify-around '>
+                            <img className='h-[80px] w-[80px] '  src="/icons/promo1.png" alt="img" />
                             <h4 className='text-[22px] text-[#191919]'>Stay Safe</h4>
-                            <p className='text-[18px] text-[#191919] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p className='text-[16px] text-[#191919] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div className='flex flex-col w-[400px] h-[220px] items-center justify-around '>
+                            <img className='h-[80px] w-[80px] '  src="/icons/promo2.png" alt="img" />
+                            <h4 className='text-[22px] text-[#191919]'>Stay Safe</h4>
+                            <p className='text-[16px] text-[#191919] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div className='flex flex-col w-[400px] h-[220px] items-center justify-around '>
+                            <img className='h-[80px] w-[80px] '  src="/icons/promo3.png" alt="img" />
+                            <h4 className='text-[22px] text-[#191919]'>Stay Safe</h4>
+                            <p className='text-[16px] text-[#191919] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         </div>
                     </div>
                 </div>
